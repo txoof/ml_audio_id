ea891e4b74bb9435b026b5edd008723bb4eca5b7
txoof <aaron.ciuffo@gmail.com>
2024-11-07 17:42:56 +0100

    add handson directory

---
+++ handson_exercises/Untitled.py
@@ -0,0 +1,15 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: ml_audio_id-venv-9ab27db4d3
+#     language: python
+#     name: ml_audio_id-venv-9ab27db4d3
+# ---
+
+
